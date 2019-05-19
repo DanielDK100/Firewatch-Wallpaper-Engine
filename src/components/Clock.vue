dc55592181ef1bae47631f7e73186df9d324d613
Daniel <danielwinther@hotmail.dk>
2019-05-19 23:10:36 +0200

    Fix

---
+++ src/components/Clock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="clock" v-show="showClock" v-text="clock"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    showClock: {
+      type: Boolean,
+    },
+    clock: {
+      type: String,
+    }
+  }
+}
+</script>
