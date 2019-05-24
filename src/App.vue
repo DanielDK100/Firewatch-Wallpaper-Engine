f4ebf05e5c4d1eb43a717578309a3a11bb2d9f33
Daniel <danielwinther@hotmail.dk>
2019-05-24 20:20:15 +0200

    Slide animation

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" :style="{backgroundImage: 'url(' + loadImage(background) + ')'}">
     <preloaded-images></preloaded-images>
     <transition name="slide-right">
-      <div id="container" v-tilt="{reverse: true, scale: 1.1}">
+      <div id="container" v-show="showWidgets" v-tilt="{reverse: true, scale: 1.1}">
         <clock @background="setBackground" :properties="properties" @properties="setProperties"></clock>
         <weather :properties="properties" @properties="setProperties"></weather>
       </div>
--- src/components/PreloadedImages.vue
+++ src/components/PreloadedImages.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="preload-images">
-    <img v-for="preloadedImage in preloadedImages" :key="preloadedImage" :src="loadImage(preloadedImage)" alt="Image">
+    <img v-for="preloadedImage in preloadedImages" :key="preloadedImage.source" :src="loadImage(preloadedImage.source)" :alt="preloadedImage.alt">
   </div>
 </template>
 
@@ -23,7 +23,7 @@ export default {
       const fromNight = moment('1_0', 'k_m')
       setTimeout(() => {
         for (let i = 1; i <= numberOfImages; i++) {
-          this.preloadedImages.push(fromNight.format('k_m'))
+          this.preloadedImages.push({source: fromNight.format('k_m'), alt: fromNight.format('k:m')})
           fromNight.add(15, 'minutes')
         }
       }, seconds)
