7a666586193c2347826c1cde22d6a4ab72f6585c
Daniel <danielwinther@hotmail.dk>
2019-06-05 20:55:43 +0200

    Added .env testing mode

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -47,10 +47,17 @@ export default {
     startClockInterval: function(roundedTime, now, duration) {
       this.setBackground(roundedTime)
       this.setClock(now)
+      if (process.env.VUE_APP_TESTING == 'true') {
+        duration = 300
+      }
       setInterval(() => {
-        this.now = moment().locale(this.locale)
+        if (process.env.VUE_APP_TESTING == 'true') {
+          this.now = this.now.add(1, 'minutes')
+        }
+        else {
+          this.now = moment().locale(this.locale)
+        }
         this.roundedTime = this.round(this.now, moment.duration(15, 'minutes'), 'ceil', 'en-gb')
-
         this.setBackground(this.roundedTime)
         this.setClock(this.now)
       }, duration)
